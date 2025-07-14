91e8629516ec81d4561955d006f40b896e79c9f5
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-07-13 20:24:07 -0500

    Implement cleanInput()

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
@@ -9,6 +10,7 @@ func main() {
 }
 
 func cleanInput(text string) []string {
-	return []string{}
+	words := strings.Fields(text)
+	return words
 }
 
